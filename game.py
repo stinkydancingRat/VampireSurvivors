fa74eac177b9b672a395291bc82525eb1de0f792
har3nz <tarikxekrem@gmail.com>
2025-03-01 14:18:46 +0300

    Player Movement

--- game.py
+++ game.py
@@ -11,13 +11,29 @@ playerImage = pygame.image.load("player.png")
 player = pygame.transform.scale(playerImage, (32, 32))
 clock = pygame.time.Clock()
 
+plrSpeed = 3
+plrX = WIDTH // 2
+plrY = HEIGHT // 2
+plrIsFacingRight = True
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        plrX -= plrSpeed
+        plrIsFacingRight = False
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        plrX += plrSpeed
+        plrIsFacingRight = True
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        plrY -= plrSpeed
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        plrY += plrSpeed
 
     window.fill((255, 255, 255))
-    window.blit(player, (WIDTH // 2, HEIGHT // 2))
+    window.blit(pygame.transform.flip(player, True, False) if not plrIsFacingRight else player, (plrX, plrY))
     pygame.display.update()
     clock.tick(60)
